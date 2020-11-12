0864dafe2515409460a4e1d47787bf414f90afee
lukamrko <lukamrko@gmail.com>
2020-11-12 21:31:38 +0100

    Futher tries to get debug to work

--- App.js
+++ App.js
@@ -43,7 +43,7 @@ const App: () => React$Node = () => {
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                Edit <Text style={styles.highlight}>App.js</Text> Promojeni ekran
                 screen and then come back to see your edits.
               </Text>
             </View>
