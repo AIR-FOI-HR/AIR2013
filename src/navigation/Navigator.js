4e1b984f013e1b724a5cfd87bc39ede6fe99b729
lukamrko <lukamrko@gmail.com>
2020-11-20 17:18:22 +0100

    Postavio sign-in unutar stacka. Sign-in je sad početni ekran.

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -2,12 +2,16 @@ import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
 
 import Home from '../screens/Home'
+import SignIn from '../screens/SignIn'
 
 const stackNavigatorOptions = {
     headerShown: false
 }
 
 const AppNavigator = createStackNavigator({
+    SignIn:{
+        screen: SignIn
+    },
     Home:{
         screen: Home
     }
---
+++ src/screens/SignIn.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+    View,
+    Text,
+} from 'react-native'
+
+
+function SignIn() {
+    return (
+        <View><Text>Pozdrav</Text></View>
+    );
+}
+
+
+export default SignIn;
