ebd77449d6cf05791c7d335554a110f14605bf3d
lukamrko <lukamrko@gmail.com>
2020-11-20 21:24:56 +0100

    Implemented Google sign-in. Need to make it work better with out app.

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,6 +11,8 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+// @react-native-community/google-signin
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 // @react-native-community/masked-view
 import org.reactnative.maskedview.RNCMaskedViewPackage;
 // react-native-gesture-handler
@@ -66,6 +68,7 @@ public class PackageList {
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
+      new RNGoogleSigninPackage(),
       new RNCMaskedViewPackage(),
       new RNGestureHandlerPackage(),
       new ReanimatedPackage(),
--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,15 +1,60 @@
-import React from 'react'
-import {
-    View,
-    Text,
-} from 'react-native'
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {GoogleSignin, GoogleSigninButton, statusCodes} from '@react-native-community/google-signin';
+
+GoogleSignin.configure({
+	webClientId: '1029055019174-akltv1ts60j361lll3v8iu7vp31ek4dm.apps.googleusercontent.com',
+	offlineAccess: true,
+});
 
 
-function SignIn() {
-    return (
-        <View><Text>Pozdrav</Text></View>
-    );
+class SignIn extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			userGoogleInfo: {},
+			loaded: false,
+		};
+	}
+
+	signIn = async () => {
+		try {
+			await GoogleSignin.hasPlayServices();
+			const userInfo = await GoogleSignin.signIn();
+			this.setState({
+				userGoogleInfo: userInfo,
+				loaded: true,
+			});
+    }
+    catch(error){
+      console.log(error.message);
+    }
+	}
+
+	render() {
+		return (
+			<View>
+				<GoogleSigninButton
+					onPress={this.signIn}
+					size={GoogleSigninButton.Size.Wide}
+					color={GoogleSigninButton.Color.Dark}
+					style={{width: 100, height: 100}}
+				/>
+				{this.state.loaded ? (
+					<View>
+						<Text>{this.state.userGoogleInfo.user.name}</Text>
+						<Text>{this.state.userGoogleInfo.user.email}</Text>
+						<Image
+							style={{width: '100', height: '100'}}
+							source={{url: this.state.userGoogleInfo.user.photo}}
+						/>
+					</View>
+				) : (
+					<Text>Not signed in</Text>
+				)}
+			</View>
+		);
+	}
 }
-
 
 export default SignIn;
