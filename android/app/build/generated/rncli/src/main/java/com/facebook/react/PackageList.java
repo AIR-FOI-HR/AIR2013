7fb0f458e54e868aa53bdd8539749338438e416f
lukamrko <lukamrko@gmail.com>
2020-11-28 18:24:02 +0100

    Buildo gradle unutar android studia nakon izmjena

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -15,6 +15,10 @@ import java.util.ArrayList;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 // @react-native-community/masked-view
 import org.reactnative.maskedview.RNCMaskedViewPackage;
+// @react-native-firebase/app
+import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
+// @react-native-firebase/auth
+import io.invertase.firebase.auth.ReactNativeFirebaseAuthPackage;
 // react-native-gesture-handler
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 // react-native-reanimated
@@ -70,6 +74,8 @@ public class PackageList {
       new MainReactPackage(mConfig),
       new RNGoogleSigninPackage(),
       new RNCMaskedViewPackage(),
+      new ReactNativeFirebaseAppPackage(),
+      new ReactNativeFirebaseAuthPackage(),
       new RNGestureHandlerPackage(),
       new ReanimatedPackage(),
       new SafeAreaContextPackage(),
