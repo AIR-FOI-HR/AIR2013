e0501d3916632c7d42e64d970aad64b4b5e2875e
spetrovic2 <61705752+spetrovic2@users.noreply.github.com>
2020-12-01 05:22:17 +0100

    Oblikovanje okvira

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -7,18 +7,17 @@ import {
     TextInput,
     TouchableOpacity,
     Switch,
-    ToggleSwitch
 } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import EntypoIcon from 'react-native-vector-icons/Entypo';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
+import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome'; 
 
 
 export default class App extends React.Component {
 
-    constructor(props) {
+    constructor(props) { //Konstruktor za switch 1 i 2
         super(props);
         this.state = {
             toggle: false,
@@ -31,7 +30,7 @@ export default class App extends React.Component {
             <ScrollView showsVerticalScrollIndicator={false} style={styles.scrollView}>
                 {/*Zaglavlje s pozdravom i implementacija slike*/}
                 <View style={styles.mainView}>
-                    <View style={styles.welcomeHeaderView}>
+                    <View style={styles.postavkeIKonf}>
                         <Text style={styles.settingsText}>Postavke i</Text>
                         <Text style={styles.settingsTextName}>konfiguracija</Text>
                     </View>
@@ -44,73 +43,74 @@ export default class App extends React.Component {
                     
                 </View>
                 
-                <View styles={styles.glavniObrub}>
-                     <View style={styles.marginaSlike}>
-                        <View style={styles.centriranje}>
-                            <Image 
-                                    source={require('../assets/icons/profile.png')}
-                                    style={styles.imageCenter}
-                                />
-                            <Text style={styles.tekstImena}> John Joe </Text>
-                        </View>
-                    </View>
-                    
-                    <View style={styles.velicinaFonta}>
+               
+                        <View style={styles.marginaSlikeIokvir}>
+                            <View style={styles.imeISlika}>
+                                <Image 
+                                        source={require('../assets/icons/profile.png')}
+                                        style={styles.imageCenter}
+                                    />
+                                <Text style={styles.tekstImena}> John Joe </Text>
+                            </View>
+                        
+                        
+                        <View style={styles.velicinaFonta}>
 
 
-                    </View>
-                        <View style={styles.margineTeksta}>
-                            
-                                    <EntypoIcon name="light-up">
-                                        <Text style={styles.tekstIzbornika}> Tamna tema </Text>
-                                        
-                                            <Switch
-                                                trackColor={{false: 'gray', true: 'teal'}}
-                                                thumbColor="white"
-                                                ios_backgroundColor="gray"
-                                                onValueChange={(value) => this.setState({toggle: value})}
-                                                value={this.state.toggle}
-                                            />  
-                     
-                                    </EntypoIcon>  
-                                                 
                         </View>
-                        <View>
-                            <IonIcon name="notifications"> 
-                                <Text style={styles.tekstIzbornika}> Obavijesti </Text>
-                                <Switch
-                                     trackColor={{false: 'gray', true: 'teal'}}
-                                    thumbColor="white"
-                                    ios_backgroundColor="gray"
-                                    onValueChange={(value) => this.setState({toggle2: value})}
-                                    value={this.state.toggle2}
-                                 /> 
+                            <View style={styles.margineTeksta}>
+                                
+                                        <EntypoIcon name="light-up">
+                                            <Text style={styles.tekstIzbornika}> Tamna tema </Text>
+                                            
+                                                <Switch
+                                                    trackColor={{false: 'gray', true: 'teal'}}
+                                                    thumbColor="white"
+                                                    ios_backgroundColor="gray"
+                                                    onValueChange={(value) => this.setState({toggle: value})}
+                                                    value={this.state.toggle}
+                                                />  
+                        
+                                        </EntypoIcon>  
+                                                    
+                            </View>
+                            <View>
+                                <IonIcon name="notifications"> 
+                                    <Text style={styles.tekstIzbornika}> Obavijesti </Text>
+                                    <Switch
+                                        trackColor={{false: 'gray', true: 'teal'}}
+                                        thumbColor="white"
+                                        ios_backgroundColor="gray"
+                                        onValueChange={(value) => this.setState({toggle2: value})}
+                                        value={this.state.toggle2}
+                                    /> 
 
-                            </IonIcon>
-                            
+                                </IonIcon>
+                                
+                            </View>
+                            <View style={styles.margineTeksta}>
+                                <EntypoIcon name="email">
+                                    <Text style={styles.tekstIzbornika}> E-mail </Text>
+                                </EntypoIcon>
+                                
+                            </View>
+                            <View >
+                                <TouchableOpacity>
+                                    <FontAwesomeIcons name="home"> 
+                                        <Text style={styles.tekstIzbornika} > Dodaj ili ukloni stavke 
+                                            <MaterialIcons name="arrow-forward-ios"></MaterialIcons> 
+                                        </Text> 
+                                    </FontAwesomeIcons>        
+                                </TouchableOpacity> 
+                            </View>
+                            <View style={styles.margineTeksta}>
+                                <TouchableOpacity>
+                                    <Text style={styles.tekstIzbornika}> Odjavi se </Text>
+                                </TouchableOpacity>
+                            </View>    
                         </View>
-                        <View style={styles.margineTeksta}>
-                            <EntypoIcon name="email">
-                                <Text style={styles.tekstIzbornika}> E-mail </Text>
-                            </EntypoIcon>
-                            
-                        </View>
-                        <View >
-                            <TouchableOpacity>
-                                <FontAwesomeIcons name="home"> 
-                                    <Text style={styles.tekstIzbornika} > Dodaj ili ukloni stavke 
-                                        <MaterialIcons name="arrow-forward-ios"></MaterialIcons> 
-                                    </Text> 
-                                </FontAwesomeIcons>        
-                            </TouchableOpacity> 
-                        </View>
-                        <View style={styles.margineTeksta}>
-                            <TouchableOpacity>
-                                <Text style={styles.tekstIzbornika}> Odjavi se </Text>
-                            </TouchableOpacity>
-                        </View>    
-                    </View>
-            
+                   
+                
             </ScrollView>
         );
     }
@@ -129,7 +129,7 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: 'space-between'
     },
-    welcomeHeaderView: {
+    postavkeIKonf: {
         width: "80%"
     },
     settingsText: {
@@ -173,32 +173,29 @@ const styles = StyleSheet.create({
         
 
     },
-    centriranje:{
+    imeISlika:{
         alignItems:"center",
         justifyContent:"center", 
+        bottom:80,
          
     },
     margineTeksta:{
         marginBottom:20,
         marginTop:20,
     },
-    marginaSlike:{
+    marginaSlikeIokvir:{
         marginBottom:40,
         marginTop:80,
-    },
-    glavniObrub:{
-        borderTopWidth:2,
-        borderBottomWidth:2,
-        borderRightWidth:2,
-        borderLeftWidth:2,
-        borderColor:"black",
-        flex:1,
-        backgroundColor:"black",
-        borderRadius: 6,
+        borderRadius:22,
+        borderWidth:2,
+        borderColor:"grey",
+        height:610,
+        justifyContent: "center",
+        bottom:50,
     },
     tekstImena:{
         marginBottom:20,
-        marginTop:20,
+        marginTop:4,
         fontSize:25,
         fontWeight: "700",
     },
@@ -207,5 +204,5 @@ const styles = StyleSheet.create({
         
     },
     
-    
+  
 })
