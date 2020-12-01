21a825ae7c584705053f7bc5209cbb7b99206831
spetrovic2 <61705752+spetrovic2@users.noreply.github.com>
2020-12-01 04:33:42 +0100

    Kreiranje samostalnih switcheva unutar "Settings"

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -5,14 +5,27 @@ import {
     Image,
     StyleSheet,
     TextInput,
-    TouchableOpacity
+    TouchableOpacity,
+    Switch,
+    ToggleSwitch
 } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import EntypoIcon from 'react-native-vector-icons/Entypo';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
+
+
 export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            toggle: false,
+            toggle2: false,
+        };
+    }
+
     render(){
         return (
             <ScrollView showsVerticalScrollIndicator={false} style={styles.scrollView}>
@@ -47,13 +60,32 @@ export default class App extends React.Component {
 
                     </View>
                         <View style={styles.margineTeksta}>
+                            
                                     <EntypoIcon name="light-up">
                                         <Text style={styles.tekstIzbornika}> Tamna tema </Text>
-                                    </EntypoIcon>                        
+                                        
+                                            <Switch
+                                                trackColor={{false: 'gray', true: 'teal'}}
+                                                thumbColor="white"
+                                                ios_backgroundColor="gray"
+                                                onValueChange={(value) => this.setState({toggle: value})}
+                                                value={this.state.toggle}
+                                            />  
+                     
+                                    </EntypoIcon>  
+                                                 
                         </View>
                         <View>
                             <IonIcon name="notifications"> 
                                 <Text style={styles.tekstIzbornika}> Obavijesti </Text>
+                                <Switch
+                                     trackColor={{false: 'gray', true: 'teal'}}
+                                    thumbColor="white"
+                                    ios_backgroundColor="gray"
+                                    onValueChange={(value) => this.setState({toggle2: value})}
+                                    value={this.state.toggle2}
+                                 /> 
+
                             </IonIcon>
                             
                         </View>
@@ -82,6 +114,7 @@ export default class App extends React.Component {
             </ScrollView>
         );
     }
+
 }
 
 const styles = StyleSheet.create({
@@ -171,6 +204,8 @@ const styles = StyleSheet.create({
     },
     tekstIzbornika:{
         fontWeight: "bold"
+        
     },
+    
     
 })
