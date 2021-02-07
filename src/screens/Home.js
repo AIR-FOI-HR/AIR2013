1b82e7f3d401c66c749c2305a38d0c6ceff83278
lukamrko <lukamrko@gmail.com>
2021-02-07 01:47:04 +0100

    Dodana varijabla koja prati koja je trenutno boja aktivna

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -23,18 +23,22 @@ import Icon from 'react-native-vector-icons/Ionicons'
 import { colors } from '../constants/DesignConstants'
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		BojeEnum = {
+			obicna: 'obicna',
+			narancasta: 'narancasta',
+			zelena: 'zelena',
+			crvena: 'crvena',
+		};
+		this.state = {
+			isLoading: true,
+			dataSourceRequests: null,
+			dataSourceClients: null,
+			trenutnaBoja: BojeEnum.obicna,
+		};
 
-    constructor(props) {
-        
-
-        super(props);
-        this.state = {
-            isLoading: true,
-            dataSourceRequests: null,
-            dataSourceClients: null,
-        }
-
-        /*
+		/*
             this.state.currentUser je googleov zapis korisnika. 
             Za pristup emailu koristiti this.state.currentUser.user.email
             Za pristup korisničkom imenu koristiti this.state.currentUser.user.name
@@ -43,170 +47,218 @@ export default class App extends React.Component {
             https://github.com/react-native-google-signin/google-signin i pogledati 3. naslov
         */
 
-        this.getCurrentUser();
+		this.getCurrentUser();
+	}
 
-    }
+	getCurrentUser = async () => {
+		const currentUser = await GoogleSignin.getCurrentUser();
+		this.setState({ currentUser });
+		console.log('Ušao u google');
+	};
 
-    getCurrentUser = async () => 
-    {
-        const currentUser = await GoogleSignin.getCurrentUser();
-        this.setState({ currentUser });
-        console.log("Ušao u google");
+	async componentDidMount() {
+		await fetch('https://jsonkeeper.com/b/R2BS')
+			.then((responseRequests) => responseRequests.json())
+			.then((responseRequestsJson) => {
+				this.setState({
+					dataSourceRequests: responseRequestsJson,
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 
-    };
+		await fetch('https://jsonkeeper.com/b/6LCM')
+			.then((responseClients) => responseClients.json())
+			.then((responseClientsJson) => {
+				this.setState({
+					isLoading: false,
+					dataSourceClients: responseClientsJson,
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
-    async componentDidMount() {
-        await fetch('https://jsonkeeper.com/b/R2BS')
-            .then((responseRequests) => responseRequests.json())
-            .then((responseRequestsJson) => {
-                this.setState({
-                    dataSourceRequests: responseRequestsJson,
-                })
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+	ProvjeriNarancastu = async () => {
+		if (this.state.trenutnaBoja != BojeEnum.narancasta) {
+			this.setState({
+				trenutnaBoja: BojeEnum.narancasta,
+			});
+		} else {
+			this.setState({
+				trenutnaBoja: BojeEnum.obicna,
+			});
+		}
+		console.log(this.state.trenutnaBoja);
+	};
 
-        await fetch('https://jsonkeeper.com/b/6LCM')
-            .then((responseClients) => responseClients.json())
-            .then((responseClientsJson) => {
-                this.setState({
-                    isLoading: false,
-                    dataSourceClients: responseClientsJson,
-                })
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
+	ProvjeriZelenu = async () => {
+		if (this.state.trenutnaBoja != BojeEnum.zelena) {
+			this.setState({
+				trenutnaBoja: BojeEnum.zelena,
+			});
+		} else {
+			this.setState({
+				trenutnaBoja: BojeEnum.obicna,
+			});
+		}
+		console.log(this.state.trenutnaBoja);
+	};
 
-    render()
-    {
-        console.log("Glavni dio");
-        console.log(this.state.currentUser); 
-        if (this.state.isLoading) {
-            return (
-                <View style={styles.mainView}>
-                    <ActivityIndicator />
-                </View>
-            );
-        } else {
+	ProvjeriCrvenu = async () => {
+		if (this.state.trenutnaBoja != BojeEnum.crvena) {
+			this.setState({
+				trenutnaBoja: BojeEnum.crvena,
+			});
+		} else {
+			this.setState({
+				trenutnaBoja: BojeEnum.obicna,
+			});
+		}
+		console.log(this.state.trenutnaBoja);
+	};
 
-            var dataRequests = this.state.dataSourceRequests;
-            var dataClients = this.state.dataSourceClients;
+	render() {
+		//console.log(this.state.currentUser);
+		console.log(this.state.trenutnaBoja);
+		if (this.state.isLoading) {
+			return (
+				<View style={styles.mainView}>
+					<ActivityIndicator />
+				</View>
+			);
+		} else {
+			var dataRequests = this.state.dataSourceRequests;
+			var dataClients = this.state.dataSourceClients;
 
-            var statusNew = 0;
-            var statusApproved = 0;
-            var statusRejected = 0;
+			var statusNew = 0;
+			var statusApproved = 0;
+			var statusRejected = 0;
 
-            let requests = dataRequests.map((requestVal, keyRequest) => {
-                var sentThrough = '';
-                var status = null;
-                if (requestVal.sent === true) {
-                    sentThrough = 'email';
-                }
-                if (requestVal.processed === false) {
-                    status = 'new';
-                    statusNew++;
-                } else if (requestVal.confirmed === true) {
-                    status = 'approved';
-                    statusApproved++;
-                } else if (requestVal.confirmed === false) {
-                    status = 'rejected';
-                    statusRejected++;
-                }
+			let requests = dataRequests.map((requestVal, keyRequest) => {
+				var sentThrough = '';
+				var status = null;
+				if (requestVal.sent === true) {
+					sentThrough = 'email';
+				}
+				if (requestVal.processed === false) {
+					status = 'new';
+					statusNew++;
+				} else if (requestVal.confirmed === true) {
+					status = 'approved';
+					statusApproved++;
+				} else if (requestVal.confirmed === false) {
+					status = 'rejected';
+					statusRejected++;
+				}
 
-                var clientId = requestVal.clientId;
-                var requestId = requestVal.requestId;
-                var property = requestVal.property;
-                var dateFrom = requestVal.fromDate;
-                var dateTo = requestVal.toDate;
-                var numberOfPeople = requestVal.numberOfPeople;
-                var responseBody = requestVal.responseBody;
+				var clientId = requestVal.clientId;
+				var requestId = requestVal.requestId;
+				var property = requestVal.property;
+				var dateFrom = requestVal.fromDate;
+				var dateTo = requestVal.toDate;
+				var numberOfPeople = requestVal.numberOfPeople;
+				var responseBody = requestVal.responseBody;
 
-                let clients = dataClients.map((clientVal) => {
-                    var clientNameSurname = '';
-                    if (clientVal.clientId === clientId) {
-                        var clientNameSurname = clientVal.name + ' ' + clientVal.surname;
-                    }
+				let clients = dataClients.map((clientVal) => {
+					var clientNameSurname = '';
+					if (clientVal.clientId === clientId) {
+						var clientNameSurname = clientVal.name + ' ' + clientVal.surname;
+					}
 
-                    return clientNameSurname;
-                });
+					return clientNameSurname;
+				});
 
-                return <View key={keyRequest}>
-                    <Request
-                        GuestName={clients}
-                        PropertyName={requestVal.property}
-                        NumberOfGuests={requestVal.numberOfPeople}
-                        Status={status}
-                        Channel={sentThrough}
-                        onPress={() => { this.props.navigation.navigate('DetailedRequest', {requestId: {requestId}, property: {property}, dateFrom: {dateFrom}, dateTo: {dateTo}, numberOfPeople: {numberOfPeople}, responseBody: {responseBody}, clients: {clients} });}}
-                    />
-                </View>
-            });
+				return (
+					<View key={keyRequest}>
+						<Request
+							GuestName={clients}
+							PropertyName={requestVal.property}
+							NumberOfGuests={requestVal.numberOfPeople}
+							Status={status}
+							Channel={sentThrough}
+							onPress={() => {
+								this.props.navigation.navigate('DetailedRequest', {
+									requestId: { requestId },
+									property: { property },
+									dateFrom: { dateFrom },
+									dateTo: { dateTo },
+									numberOfPeople: { numberOfPeople },
+									responseBody: { responseBody },
+									clients: { clients },
+								});
+							}}
+						/>
+					</View>
+				);
+			});
 
-            return (
-                <ScrollView showsVerticalScrollIndicator={false} style={styles.scrollView}>
-                    {/*Zaglavlje s pozdravnom porukom i ikonom korisničkog profila*/}
-                    <View style={styles.mainView}>
-                        <View style={styles.welcomeHeaderView}>
-                            <Text style={styles.txtWelcome}>Dobrodošao,</Text>
-                            <Text style={styles.txtWelcomeName}>{this.state.currentUser.user.name}</Text>
-                        </View>
+			return (
+				<ScrollView showsVerticalScrollIndicator={false} style={styles.scrollView}>
+					{/*Zaglavlje s pozdravnom porukom i ikonom korisničkog profila*/}
+					<View style={styles.mainView}>
+						<View style={styles.welcomeHeaderView}>
+							<Text style={styles.txtWelcome}>Dobrodošao,</Text>
+							<Text style={styles.txtWelcomeName}>{this.state.currentUser.user.name}</Text>
+						</View>
 
-                        <View style={styles.profileIconView}>
-                            <TouchableHighlight
-                                onPress={() => this.props.navigation.navigate('Settings')}>
-                                <Image
-                                    source={require('../assets/icons/profile.png')}
-                                    style={styles.profileIconImage}
-                                />
-                            </TouchableHighlight>
+						<View style={styles.profileIconView}>
+							<TouchableHighlight onPress={() => this.props.navigation.navigate('Settings')}>
+								<Image
+									source={require('../assets/icons/profile.png')}
+									style={styles.profileIconImage}
+								/>
+							</TouchableHighlight>
+						</View>
+					</View>
 
-                        </View>
-                    </View>
+					{/*Polje za pretraživanje i gumb za sortiranje*/}
+					<View style={styles.mainView}>
+						<View style={styles.searchBoxView}>
+							<Icon name="search" size={15} color="#789789" />
+							<TextInput placeholder="Pretraži..." style={styles.searchBoxInputField} />
+						</View>
+						<View style={styles.sortView}>
+							<Icon name="funnel-outline" size={15} color="#789789" />
+						</View>
+					</View>
 
-                    {/*Polje za pretraživanje i gumb za sortiranje*/}
-                    <View style={styles.mainView}>
-                        <View style={styles.searchBoxView}>
-                            <Icon name="search" size={15} color="#789789" />
-                            <TextInput placeholder="Pretraži..." style={styles.searchBoxInputField} />
-                        </View>
-                        <View style={styles.sortView}>
-                            <Icon name="funnel-outline" size={15} color="#789789" />
-                        </View>
-                    </View>
+					{/*Color boxes*/}
+					<View style={styles.mainView}>
+						<TouchableHighlight onPress={this.ProvjeriNarancastu}>
+							<View style={styles.colorBoxOrange}>
+								<Text style={styles.colorBoxesTextLabel}>novi</Text>
+								<Text style={styles.colorBoxesTextNumber}>{statusNew}</Text>
+							</View>
+						</TouchableHighlight>
 
-                    {/*Color boxes*/}
-                    <View style={styles.mainView}>
-                        <View style={styles.colorBoxOrange}>
-                            <Text style={styles.colorBoxesTextLabel}>novi</Text>
-                            <Text style={styles.colorBoxesTextNumber}>{statusNew}</Text>
-                        </View>
-                        <View style={styles.colorBoxGreen}>
-                            <Text style={styles.colorBoxesTextLabel}>odobreni</Text>
-                            <Text style={styles.colorBoxesTextNumber}>{statusApproved}</Text>
-                        </View>
-                        <View style={styles.colorBoxRed}>
-                            <Text style={styles.colorBoxesTextLabel}>odbijeni</Text>
-                            <Text style={styles.colorBoxesTextNumber}>{statusRejected}</Text>
-                        </View>
-                    </View>
+						<TouchableHighlight onPress={this.ProvjeriZelenu}>
+							<View style={styles.colorBoxGreen}>
+								<Text style={styles.colorBoxesTextLabel}>odobreni</Text>
+								<Text style={styles.colorBoxesTextNumber}>{statusApproved}</Text>
+							</View>
+						</TouchableHighlight>
 
+						<TouchableHighlight onPress={this.ProvjeriCrvenu}>
+							<View style={styles.colorBoxRed}>
+								<Text style={styles.colorBoxesTextLabel}>odbijeni</Text>
+								<Text style={styles.colorBoxesTextNumber}>{statusRejected}</Text>
+							</View>
+						</TouchableHighlight>
+					</View>
 
-                    {/*Requests*/}
-                    <View style={styles.requestView}>
-                        <Text style={styles.txtWelcome}>Request overview</Text>
+					{/*Requests*/}
+					<View style={styles.requestView}>
+						<Text style={styles.txtWelcome}>Request overview</Text>
 
-                        {requests}
-
-                    </View>
-
-                </ScrollView>
-
-            );
-        }
-    }
+						{requests}
+					</View>
+				</ScrollView>
+			);
+		}
+	}
 }
 
 const styles = StyleSheet.create({
