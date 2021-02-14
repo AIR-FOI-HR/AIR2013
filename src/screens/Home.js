b41a5fac7fba1f5f812f78b7976b44ac30b2576a
lukamrko <lukamrko@gmail.com>
2021-02-14 16:05:29 +0100

    Dodani filteri po boji i po apartmanima

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,23 +1,15 @@
-import React from 'react'
-import {
-	View,
-	Text,
-	Image,
-	StyleSheet,
-	TextInput,
-	TouchableHighlight,
-	ActivityIndicator,
-} from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler'
+import React from 'react';
+import { View, Text, Image, StyleSheet, TextInput, TouchableHighlight, ActivityIndicator } from 'react-native';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 
 import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-community/google-signin';
 
-import Request from '../components/Request'
+import Request from '../components/Request';
 
-import Icon from 'react-native-vector-icons/Ionicons'
+import Icon from 'react-native-vector-icons/Ionicons';
 
-import { colors } from '../constants/DesignConstants'
-import { FetchDataFromAPI } from '../backend/ApiConnection'
+import { colors } from '../constants/DesignConstants';
+import { FetchDataFromAPI } from '../backend/ApiConnection';
 
 export default class App extends React.Component {
 	constructor(props) {
@@ -26,7 +18,10 @@ export default class App extends React.Component {
 			isLoading: true,
 			dataSourceRequests: null,
 			dataSourceClients: null,
+			dataSourceProperties: null,
 			clientEmail: null,
+			requestFilter: undefined,
+			propertyFilter: undefined,
 		};
 		/*
 			this.state.currentUser je googleov zapis korisnika. 
@@ -41,24 +36,28 @@ export default class App extends React.Component {
 
 	urlClients = 'https://air2020api.azure-api.net/api/Clients';
 	urlRequests = 'https://air2020api.azure-api.net/api/Requests';
+	urlProperties = 'https://air2020api.azure-api.net/api/Properties';
 
 	async componentDidMount() {
 		this.setState({
 			dataSourceRequests: await FetchDataFromAPI(this.urlRequests),
 			dataSourceClients: await FetchDataFromAPI(this.urlClients),
+			dataSourceProperties: await FetchDataFromAPI(this.urlProperties),
 			isLoading: false,
+			requestFilter: undefined,
+			propertyFilter: undefined,
 		});
 
-		this.didFocusSubscription = this.props.navigation.addListener(
-			'willFocus',
-			async () => {
-				this.setState({
-					dataSourceRequests: await FetchDataFromAPI(this.urlRequests),
-					dataSourceClients: await FetchDataFromAPI(this.urlClients),
-					isLoading: false,
-				})
-			}
-		);
+		this.didFocusSubscription = this.props.navigation.addListener('willFocus', async () => {
+			this.setState({
+				dataSourceRequests: await FetchDataFromAPI(this.urlRequests),
+				dataSourceClients: await FetchDataFromAPI(this.urlClients),
+				dataSourceProperties: await FetchDataFromAPI(this.urlProperties),
+				isLoading: false,
+				requestFilter: undefined,
+				propertyFilter: undefined,
+			});
+		});
 	}
 
 	componentWillUnmount() {
@@ -75,7 +74,7 @@ export default class App extends React.Component {
 		var user = '';
 		try {
 			user = this.state.currentUser.user.name;
-		} catch (error) { }
+		} catch (error) {}
 		if (this.state.isLoading) {
 			return (
 				<View style={styles.mainView}>
@@ -85,10 +84,38 @@ export default class App extends React.Component {
 		} else {
 			var dataRequests = this.state.dataSourceRequests;
 			var dataClients = this.state.dataSourceClients;
+			var dataProperties = this.state.dataSourceProperties;
 
 			var statusNew = 0;
 			var statusApproved = 0;
 			var statusRejected = 0;
+
+			let properties = dataProperties.map((propertyVal, keyProperty) => {
+				var propertyName = propertyVal.name;
+
+				return (
+					<View key={keyProperty}>
+						<TouchableOpacity
+							style={styles.btnPropertyFilter}
+							onPress={() => {
+								if (this.state.propertyFilter === propertyName) {
+									this.setState({
+										propertyFilter: undefined,
+									});
+									console.log(this.state.propertyFilter);
+								} else {
+									this.setState({
+										propertyFilter: propertyName,
+									});
+									console.log(this.state.propertyFilter);
+								}
+							}}
+						>
+							<Text style={styles.btnText2}>{propertyName}</Text>
+						</TouchableOpacity>
+					</View>
+				);
+			});
 
 			let requests = dataRequests.map((requestVal, keyRequest) => {
 				var sentThrough = '';
@@ -123,52 +150,152 @@ export default class App extends React.Component {
 				var responseBody = requestVal.responseBody;
 				var clientId = requestVal.clientId;
 				var client = requestVal.client;
-				var clientEmail = ""
+				var clientEmail = '';
 
 				let clients = dataClients.map((clientVal, keyClient) => {
 					var clientNameSurname = '';
 					if (clientVal.clientId === clientId) {
 						var clientNameSurname = clientVal.name + ' ' + clientVal.surname;
-						clientEmail = clientVal.email
+						clientEmail = clientVal.email;
 					}
 
 					return clientNameSurname;
 				});
 
-				return (
-					<View key={keyRequest}>
-						<Request
-							GuestName={clients}
-							PropertyName={requestVal.property}
-							NumberOfGuests={requestVal.numberOfPeople}
-							Status={status}
-							Channel={sentThrough}
-							onPress={() => {
-								this.props.navigation.navigate('DetailedRequest', {
-									requestId: { requestId },
-									property: { property },
-									unit: { unit },
-									dateFrom: { dateFrom },
-									dateTo: { dateTo },
-									priceUponRequest: { priceUponRequest },
-									confirmed: { confirmed },
-									processed: { processed },
-									sent: { sent },
-									numberOfPeople: { numberOfPeople },
-									responseSubject: { responseSubject },
-									responseBody: { responseBody },
-									clientId: { clientId },
-									client: { client },
-									clients: { clients },
-									clientEmail: { clientEmail }
-
-								});
-							}}
-						/>
-					</View>
-				);
+				if (this.state.requestFilter === undefined) {
+					if (this.state.propertyFilter === undefined) {
+						return (
+							<View key={keyRequest}>
+								<Request
+									GuestName={clients}
+									PropertyName={requestVal.property}
+									NumberOfGuests={requestVal.numberOfPeople}
+									Status={status}
+									Channel={sentThrough}
+									onPress={() => {
+										this.props.navigation.navigate('DetailedRequest', {
+											requestId: { requestId },
+											property: { property },
+											unit: { unit },
+											dateFrom: { dateFrom },
+											dateTo: { dateTo },
+											priceUponRequest: { priceUponRequest },
+											confirmed: { confirmed },
+											processed: { processed },
+											sent: { sent },
+											numberOfPeople: { numberOfPeople },
+											responseSubject: { responseSubject },
+											responseBody: { responseBody },
+											clientId: { clientId },
+											client: { client },
+											clients: { clients },
+											clientEmail: { clientEmail },
+										});
+									}}
+								/>
+							</View>
+						);
+					} else if (requestVal.property === this.state.propertyFilter) {
+						return (
+							<View key={keyRequest}>
+								<Request
+									GuestName={clients}
+									PropertyName={requestVal.property}
+									NumberOfGuests={requestVal.numberOfPeople}
+									Status={status}
+									Channel={sentThrough}
+									onPress={() => {
+										this.props.navigation.navigate('DetailedRequest', {
+											requestId: { requestId },
+											property: { property },
+											unit: { unit },
+											dateFrom: { dateFrom },
+											dateTo: { dateTo },
+											priceUponRequest: { priceUponRequest },
+											confirmed: { confirmed },
+											processed: { processed },
+											sent: { sent },
+											numberOfPeople: { numberOfPeople },
+											responseSubject: { responseSubject },
+											responseBody: { responseBody },
+											clientId: { clientId },
+											client: { client },
+											clients: { clients },
+											clientEmail: { clientEmail },
+										});
+									}}
+								/>
+							</View>
+						);
+					}
+				} else if (status === this.state.requestFilter) {
+					if (this.state.propertyFilter === undefined) {
+						return (
+							<View key={keyRequest}>
+								<Request
+									GuestName={clients}
+									PropertyName={requestVal.property}
+									NumberOfGuests={requestVal.numberOfPeople}
+									Status={status}
+									Channel={sentThrough}
+									onPress={() => {
+										this.props.navigation.navigate('DetailedRequest', {
+											requestId: { requestId },
+											property: { property },
+											unit: { unit },
+											dateFrom: { dateFrom },
+											dateTo: { dateTo },
+											priceUponRequest: { priceUponRequest },
+											confirmed: { confirmed },
+											processed: { processed },
+											sent: { sent },
+											numberOfPeople: { numberOfPeople },
+											responseSubject: { responseSubject },
+											responseBody: { responseBody },
+											clientId: { clientId },
+											client: { client },
+											clients: { clients },
+											clientEmail: { clientEmail },
+										});
+									}}
+								/>
+							</View>
+						);
+					} else if (requestVal.property === this.state.propertyFilter) {
+						return (
+							<View key={keyRequest}>
+								<Request
+									GuestName={clients}
+									PropertyName={requestVal.property}
+									NumberOfGuests={requestVal.numberOfPeople}
+									Status={status}
+									Channel={sentThrough}
+									onPress={() => {
+										this.props.navigation.navigate('DetailedRequest', {
+											requestId: { requestId },
+											property: { property },
+											unit: { unit },
+											dateFrom: { dateFrom },
+											dateTo: { dateTo },
+											priceUponRequest: { priceUponRequest },
+											confirmed: { confirmed },
+											processed: { processed },
+											sent: { sent },
+											numberOfPeople: { numberOfPeople },
+											responseSubject: { responseSubject },
+											responseBody: { responseBody },
+											clientId: { clientId },
+											client: { client },
+											clients: { clients },
+											clientEmail: { clientEmail },
+										});
+									}}
+								/>
+							</View>
+						);
+					}
+				}
 			});
-
 
 			return (
 				<ScrollView showsVerticalScrollIndicator={false} style={styles.scrollView}>
@@ -180,41 +307,82 @@ export default class App extends React.Component {
 						</View>
 
 						<View style={styles.profileIconView}>
-							<TouchableHighlight onPress={() => this.props.navigation.navigate('Settings')}>
+							<TouchableOpacity onPress={() => this.props.navigation.navigate('Settings')}>
 								<Image
 									source={require('../assets/icons/profile.png')}
 									style={styles.profileIconImage}
 								/>
-							</TouchableHighlight>
+							</TouchableOpacity>
 						</View>
 					</View>
 
 					{/*Polje za pretraživanje i gumb za sortiranje*/}
-					<View style={styles.mainView}>
+					{/*<View style={styles.mainView}>
 						<View style={styles.searchBoxView}>
 							<Icon name="search" size={15} color="#789789" />
 							<TextInput placeholder="Pretraži..." style={styles.searchBoxInputField} />
 						</View>
-						<View style={styles.sortView}>
+						<TouchableOpacity style={styles.sortView}>
 							<Icon name="funnel-outline" size={15} color="#789789" />
-						</View>
-					</View>
+						</TouchableOpacity>
+					</View>*/}
 
 					{/*Color boxes*/}
 					<View style={styles.mainView}>
-						<View style={styles.colorBoxOrange}>
+						<TouchableOpacity
+							style={styles.colorBoxOrange}
+							onPress={() => {
+								if (this.state.requestFilter === 'new') {
+									this.setState({
+										requestFilter: undefined,
+									});
+								} else {
+									this.setState({
+										requestFilter: 'new',
+									});
+								}
+							}}
+						>
 							<Text style={styles.colorBoxesTextLabel}>novi</Text>
 							<Text style={styles.colorBoxesTextNumber}>{statusNew}</Text>
-						</View>
-						<View style={styles.colorBoxGreen}>
+						</TouchableOpacity>
+						<TouchableOpacity
+							style={styles.colorBoxGreen}
+							onPress={() => {
+								if (this.state.requestFilter === 'approved') {
+									this.setState({
+										requestFilter: undefined,
+									});
+								} else {
+									this.setState({
+										requestFilter: 'approved',
+									});
+								}
+							}}
+						>
 							<Text style={styles.colorBoxesTextLabel}>odobreni</Text>
 							<Text style={styles.colorBoxesTextNumber}>{statusApproved}</Text>
-						</View>
-						<View style={styles.colorBoxRed}>
+						</TouchableOpacity>
+						<TouchableOpacity
+							style={styles.colorBoxRed}
+							onPress={() => {
+								if (this.state.requestFilter === 'rejected') {
+									this.setState({
+										requestFilter: undefined,
+									});
+								} else {
+									this.setState({
+										requestFilter: 'rejected',
+									});
+								}
+							}}
+						>
 							<Text style={styles.colorBoxesTextLabel}>odbijeni</Text>
 							<Text style={styles.colorBoxesTextNumber}>{statusRejected}</Text>
-						</View>
+						</TouchableOpacity>
 					</View>
+
+					<View style={styles.requestView}>{properties}</View>
 
 					{/*Requests*/}
 					<View style={styles.requestView}>
@@ -232,59 +400,74 @@ const styles = StyleSheet.create({
 	scrollView: {
 		backgroundColor: colors.white,
 		paddingHorizontal: 20,
-		height: '100%'
+		height: '100%',
 	},
 	mainView: {
-		flexDirection: "row",
-		width: "100%",
+		flexDirection: 'row',
+		width: '100%',
 		marginTop: 40,
-		alignItems: "center",
-		justifyContent: 'space-between'
+		alignItems: 'center',
+		justifyContent: 'space-between',
 	},
 	welcomeHeaderView: {
-		width: "80%"
+		width: '80%',
 	},
 	txtWelcome: {
 		fontSize: 30,
-		fontWeight: "700"
+		fontWeight: '700',
 	},
 	txtWelcomeName: {
 		fontSize: 30,
-		fontWeight: "100"
+		fontWeight: '100',
 	},
 	profileIconView: {
-		width: "20%",
-		alignItems: "flex-end"
+		width: '20%',
+		alignItems: 'flex-end',
 	},
 	profileIconImage: {
 		width: 50,
-		height: 50
+		height: 50,
 	},
 	searchBoxView: {
-		flexDirection: "row",
-		alignItems: "center",
+		flexDirection: 'row',
+		alignItems: 'center',
 		elevation: 3,
-		width: "85%",
+		width: '83%',
 		backgroundColor: colors.white,
 		paddingHorizontal: 20,
 		height: 35,
 		borderRadius: 10,
-		marginLeft: 1
+		marginLeft: 1,
 	},
 	searchBoxInputField: {
 		paddingHorizontal: 10,
-		fontSize: 12
+		fontSize: 12,
 	},
 	sortView: {
-		alignItems: "center",
+		alignItems: 'center',
 		elevation: 2,
-		width: "15%",
+		width: 50,
 		backgroundColor: colors.white,
-		marginLeft: 5,
+		marginLeft: 3,
 		height: 35,
 		borderRadius: 10,
-		marginLeft: 1,
-		justifyContent: "center"
+		marginRight: 5,
+		justifyContent: 'center',
+	},
+	btnPropertyFilter: {
+		borderColor: colors.tertiary,
+		backgroundColor: colors.white,
+		borderRadius: 8,
+		borderWidth: 2,
+		padding: 8,
+		width: '40%',
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+	},
+	btnText2: {
+		fontWeight: 'bold',
+		fontSize: 14,
+		color: colors.black,
 	},
 	colorBoxOrange: {
 		backgroundColor: colors.yellow,
@@ -292,9 +475,9 @@ const styles = StyleSheet.create({
 		width: 100,
 		borderRadius: 10,
 		padding: 15,
-		justifyContent: "center",
-		alignItems: "center",
-		elevation: 5
+		justifyContent: 'center',
+		alignItems: 'center',
+		elevation: 5,
 	},
 	colorBoxGreen: {
 		backgroundColor: colors.green,
@@ -302,9 +485,9 @@ const styles = StyleSheet.create({
 		width: 100,
 		borderRadius: 10,
 		padding: 15,
-		justifyContent: "center",
-		alignItems: "center",
-		elevation: 5
+		justifyContent: 'center',
+		alignItems: 'center',
+		elevation: 5,
 	},
 	colorBoxRed: {
 		backgroundColor: colors.red,
@@ -312,9 +495,9 @@ const styles = StyleSheet.create({
 		width: 100,
 		borderRadius: 10,
 		padding: 15,
-		justifyContent: "center",
-		alignItems: "center",
-		elevation: 5
+		justifyContent: 'center',
+		alignItems: 'center',
+		elevation: 5,
 	},
 	colorBoxesTextLabel: {
 		color: colors.white,
@@ -322,12 +505,12 @@ const styles = StyleSheet.create({
 	},
 	colorBoxesTextNumber: {
 		color: colors.white,
-		fontSize: 45
+		fontSize: 45,
 	},
 	requestView: {
-		flexDirection: "column",
-		width: "100%",
+		flexDirection: 'column',
+		width: '100%',
 		marginTop: 40,
-		justifyContent: 'space-between'
-	}
-})
+		justifyContent: 'space-between',
+	},
+});
