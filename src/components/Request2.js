9a1904a8000b2dd1c1ef7d6a2a00aa379a50e4ab
spetrovic2 <61705752+spetrovic2@users.noreply.github.com>
2021-02-16 01:29:45 +0100

    Implementacija buttona za dizajn kartica i redizajn "Settings.js"

---
+++ src/components/Request2.js
@@ -0,0 +1,183 @@
+import React from "react";
+import { View, Text, Image, StyleSheet, TextInput } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { colors } from "../constants/DesignConstants";
+
+{
+  /* Funkcija koja ovisno o primljenim parametrima postavlja odgovarajuću ikonu
+    kartice zahtjeva te postavlja odgovarajuću signalnu boju ikone.
+*/
+}
+function setProperColor(requestStatus, sentThrough) {
+  if (requestStatus === "new") {
+    if (sentThrough === "email") {
+      return (
+        <Icon
+          name="mail-unread-outline"
+          size={35}
+          color={colors.yellow}
+          style={{ elevation: 5 }}
+        />
+      );
+    } else {
+      return (
+        <Icon
+          name="earth-outline"
+          size={35}
+          color={colors.yellow}
+          style={{ elevation: 5 }}
+        />
+      );
+    }
+  } else if (requestStatus === "approved") {
+    if (sentThrough === "email") {
+      return (
+        <Icon
+          name="mail-unread-outline"
+          size={35}
+          color={colors.green}
+          style={{ elevation: 5 }}
+        />
+      );
+    } else {
+      return (
+        <Icon
+          name="earth-outline"
+          size={35}
+          color={colors.green}
+          style={{ elevation: 5 }}
+        />
+      );
+    }
+  } else if (requestStatus === "rejected") {
+    if (sentThrough === "email") {
+      return (
+        <Icon
+          name="mail-unread-outline"
+          size={35}
+          color={colors.red}
+          style={{ elevation: 5 }}
+        />
+      );
+    } else {
+      return (
+        <Icon
+          name="earth-outline"
+          size={35}
+          color={colors.red}
+          style={{ elevation: 5 }}
+        />
+      );
+    }
+  }
+}
+
+{
+  /* Funkcija koja ovisno o primljenim parametrima postavlja odgovarajuću signalnu boju okvira.
+   */
+}
+function dynamicFrameColor(requestStatus) {
+  if (requestStatus === "new") {
+    return {
+      flexDirection: "row",
+      alignItems: "center",
+      elevation: 3,
+      width: "99.5%",
+      backgroundColor: "#FFF",
+      paddingHorizontal: 20,
+      height: 100,
+      borderRadius: 10,
+      marginLeft: 1,
+      marginTop: 10,
+      marginBottom: 10,
+      borderWidth: 1,
+      borderColor: colors.yellow,
+      justifyContent: "space-between",
+    };
+  } else if (requestStatus === "approved") {
+    return {
+      flexDirection: "row",
+      alignItems: "center",
+      elevation: 3,
+      width: "99.5%",
+      backgroundColor: "#FFF",
+      paddingHorizontal: 20,
+      height: 100,
+      borderRadius: 10,
+      marginLeft: 1,
+      marginTop: 10,
+      marginBottom: 10,
+      borderWidth: 1,
+      borderColor: colors.green,
+      justifyContent: "space-between",
+    };
+  } else {
+    return {
+      flexDirection: "row",
+      alignItems: "center",
+      elevation: 3,
+      width: "99.5%",
+      backgroundColor: "#FFF",
+      paddingHorizontal: 20,
+      height: 100,
+      borderRadius: 10,
+      marginLeft: 1,
+      marginTop: 10,
+      marginBottom: 10,
+      borderWidth: 1,
+      borderColor: colors.red,
+      justifyContent: "space-between",
+    };
+  }
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <TouchableOpacity
+        onPress={this.props.onPress}
+        style={dynamicFrameColor(this.props.Status)}
+      >
+        <View>
+          <Image
+            source={require("../assets/icons/profile.png")}
+            style={styles.profileIconImage}
+          />
+        </View>
+        <View style={{ width: "73%" }}>
+          <Text style={styles.txtNameSurname}>{this.props.GuestName}</Text>
+          <Text style={styles.txtPropertyName}>{this.props.PropertyName}</Text>
+          <Text style={styles.txtPeopleNumber}>
+            for {this.props.NumberOfGuests}
+          </Text>
+        </View>
+        <View>{setProperColor(this.props.Status, this.props.Channel)}</View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  profileIconImage: {
+    width: 40,
+    height: 40,
+    marginRight: 10,
+    borderRadius: 17,
+  },
+  txtNameSurname: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#353b48",
+  },
+  txtPropertyName: {
+    fontSize: 10,
+    fontWeight: "300",
+    color: "#353b48",
+  },
+  txtPeopleNumber: {
+    fontSize: 10,
+    fontWeight: "300",
+    color: "#353b48",
+  },
+});
--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -7,6 +7,7 @@ import {
     TextInput,
     TouchableOpacity,
     Switch,
+    TouchableHighlight
 } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import {GoogleSignin, Button, statusCodes} from '@react-native-community/google-signin';
@@ -14,8 +15,8 @@ import EntypoIcon from 'react-native-vector-icons/Entypo';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome'; 
-
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { colors } from "../constants/DesignConstants";
 export default class App extends React.Component {
 
     constructor(props) { //Konstruktor za switch 1 i 2
@@ -61,242 +62,373 @@ export default class App extends React.Component {
                 imgSrc = this.state.currentUser.user.photo;
             }
 		} catch(error) {};
+        
         return (
-			<ScrollView showsVerticalScrollIndicator={false} style={styles.scrollView}>
-				{/*Zaglavlje s pozdravom i implementacija slike*/}
-				<View style={styles.mainView}>
-					<View style={styles.postavkeIKonf}>
-						<Text style={styles.settingsText}>Postavke i</Text>
-						<Text style={styles.settingsTextName}>konfiguracija</Text>
-					</View>
-					<View style={styles.profileIconView}>
-						<Image source={{ uri: imgSrc}} style={styles.profileIconImage} />
-					</View>
-					{/*Profilna fotografija, ime i prezime korisnika*/}
-				</View>
-				<View style={styles.marginaSlikeIokvir}>
-					<View style={styles.imeISlika}>
-						<Image source={{ uri: imgSrc }} style={styles.imageCenter} />
-						<Text style={styles.tekstImena}> {ime} </Text>
-					</View>
-					<View style={styles.velicinaFonta}>{/*Izbornik sa switchem i ikonicama*/}</View>
-					<View style={styles.margineTeksta}>
-						<EntypoIcon name="light-up" size={22} style={styles.lightUp}></EntypoIcon>
-						<View>
-							<View style={styles.darkMode}>
-								<Text style={styles.tekstIzbornika}> Tamna tema </Text>
-							</View>
-							<View style={styles.switchPlace1}>
-								<Switch
-									trackColor={{ false: 'gray', true: 'teal' }}
-									thumbColor="white"
-									ios_backgroundColor="gray"
-									onValueChange={(value) => this.setState({ toggle: value })}
-									value={this.state.toggle}
-								/>
-							</View>
-						</View>
-					</View>
-					<View style={styles.margineTeksta}>
-						<View style={styles.notificationIcon}>
-							<IonIcon name="notifications" size={22}></IonIcon>
-						</View>
-						<View style={styles.notificationTxt}>
-							<Text style={styles.tekstIzbornika}> Obavijesti </Text>
-						</View>
-						<View style={styles.switchPlace2}>
-							<Switch
-								trackColor={{ false: 'gray', true: 'teal' }}
-								thumbColor="white"
-								ios_backgroundColor="gray"
-								onValueChange={(value) => this.setState({ toggle2: value })}
-								value={this.state.toggle2}
-							/>
-						</View>
-					</View>
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            style={styles.scrollView}
+          >
+            {/*Zaglavlje s pozdravom i implementacija slike*/}
+            <View style={styles.mainView}>
+              <View style={styles.postavkeIKonf}>
+                <Text style={styles.settingsText}>Postavke i</Text>
+                <Text style={styles.settingsTextName}>konfiguracija</Text>
+              </View>
+              <View style={styles.profileIconView}>
+                <TouchableOpacity
+                  onPress={() => this.props.navigation.navigate("Home")}
+                >
+                  <Image
+                    source={{ uri: imgSrc }}
+                    style={styles.profileIconImage}
+                  />
+                </TouchableOpacity>
+              </View>
+              {/*Profilna fotografija, ime i prezime korisnika*/}
+            </View>
+            <View style={styles.marginaSlikeIokvir}>
+              <View style={styles.imeISlika}>
+                <Image source={{ uri: imgSrc }} style={styles.imageCenter} />
+                <Text style={styles.tekstImena}> {ime} </Text>
+              </View>
+              <View style={styles.velicinaFonta}>
+                {/*Izbornik sa switchem i ikonicama*/}
+              </View>
+              <View style={styles.margineTeksta}>
+                <EntypoIcon
+                  name="light-up"
+                  size={22}
+                  style={styles.lightUp}
+                ></EntypoIcon>
+                <View>
+                  <View style={styles.darkMode}>
+                    <Text style={styles.tekstIzbornika}> Tamna tema </Text>
+                  </View>
+                  <View style={styles.switchPlace1}>
+                    <Switch
+                      trackColor={{ false: "gray", true: "teal" }}
+                      thumbColor="white"
+                      ios_backgroundColor="gray"
+                      onValueChange={(value) =>
+                        this.setState({ toggle: value })
+                      }
+                      value={this.state.toggle}
+                    />
+                  </View>
+                </View>
+              </View>
+              <View style={styles.margineTeksta}>
+                <View style={styles.notificationIcon}>
+                  <IonIcon name="notifications" size={22}></IonIcon>
+                </View>
+                <View style={styles.notificationTxt}>
+                  <Text style={styles.tekstIzbornika}> Obavijesti </Text>
+                </View>
+                <View style={styles.switchPlace2}>
+                  <Switch
+                    trackColor={{ false: "gray", true: "teal" }}
+                    thumbColor="white"
+                    ios_backgroundColor="gray"
+                    onValueChange={(value) => this.setState({ toggle2: value })}
+                    value={this.state.toggle2}
+                  />
+                </View>
+              </View>
 
-					<View style={styles.emailStavke}>
-						<View style={styles.margineTeksta}>
-							<EntypoIcon name="email" size={20}>
-								<Text style={styles.tekstIzbornika}> {email}</Text>
-							</EntypoIcon>
-						</View>
+              <View style={styles.emailStavke}>
+                <View style={styles.margineTeksta}>
+                  <EntypoIcon name="email" size={20}>
+                    <Text style={styles.tekstIzbornika}> {email}</Text>
+                  </EntypoIcon>
+                </View>
 
-						<View style={styles.margineTeksta}>
-							<TouchableOpacity
-								style={styles.dodajUkloniStavke}
-								onPress={() => this.props.navigation.navigate('AddAparOrTemp')}
-							>
-								<FontAwesomeIcons name="home" size={24}>
-									<Text style={styles.tekstIzbornika}> Dodaj ili ukloni stavke </Text>
-								</FontAwesomeIcons>
-								<View style={styles.arrow}>
-									<MaterialIcons name="arrow-forward-ios" size={25}></MaterialIcons>
-								</View>
-							</TouchableOpacity>
-						</View>
+                <View style={styles.margineTeksta}>
+                  <TouchableOpacity
+                    style={styles.dodajUkloniStavke}
+                    onPress={() =>
+                      this.props.navigation.navigate("AddAparOrTemp")
+                    }
+                  >
+                    <FontAwesomeIcons name="home" size={24}>
+                      <Text style={styles.tekstIzbornika}>
+                        {" "}
+                        Dodaj ili ukloni stavke{" "}
+                      </Text>
+                    </FontAwesomeIcons>
+                    <View style={styles.arrow}>
+                      <MaterialIcons
+                        name="arrow-forward-ios"
+                        size={25}
+                      ></MaterialIcons>
+                    </View>
+                  </TouchableOpacity>
+                </View>
 
-						<View style={styles.margineTeksta}>
-							<TouchableOpacity
-								onPress={() => {
-									this.signOut();
-								}}
-							>
-								<View style={styles.logoutIcon}>
-									<MaterialIcons name="logout" size={24}></MaterialIcons>
-								</View>
-								<Text style={styles.tekstIzbornika}> Odjavi se </Text>
-							</TouchableOpacity>
-						</View>
-					</View>
-					<View style={styles.foi}>
-						<Text style={styles.foiText}>Made @</Text>
-						<Image source={require('../assets/images/foiLogo.png')}></Image>
-					</View>
-				</View>
-			</ScrollView>
-		);
+                <View style={styles.margineOdjave}>
+                  <TouchableOpacity
+                    style={styles.opacityOdjava}
+                    onPress={() => {
+                      this.signOut();
+                    }}
+                  >
+                    <View style={styles.logoutIcon}>
+                      <MaterialIcons name="logout" size={24}></MaterialIcons>
+                    </View>
+                    <Text style={styles.tekstIzbornika}> Odjavi se </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                borderBottomColor: "black",
+                borderBottomWidth: 1,
+                bottom: 340,
+              }}
+            />
+            <View
+              style={{
+                borderBottomColor: "black",
+                borderBottomWidth: 1,
+                bottom: 210,
+              }}
+            />
+            <Text style={styles.dizajnKarticaTekst}>
+              Odaberite dizajn kartica po želji:
+            </Text>
+            <View style={styles.txtButtonIcon}>
+              <View style={styles.btn1}>
+                <TouchableHighlight
+                  style={styles.btnBorder1}
+                  onPress={() => this.changeBackgroundColor(colors.tertiary)}
+                >
+                  <Text style={styles.btnText1}>DIZAJN 1</Text>
+                </TouchableHighlight>
+              </View>
+
+              <View style={styles.btn2}>
+                <TouchableHighlight
+                  style={styles.btnBorder2}
+                  onPress={async () => {}}
+                >
+                  <Text style={styles.btnText2}>DIZAJN 2</Text>
+                </TouchableHighlight>
+              </View>
+            </View>
+            <View style={styles.foi}>
+              <Text style={styles.foiText}>Made @</Text>
+              <Image source={require("../assets/images/foiLogo.png")}></Image>
+            </View>
+          </ScrollView>
+        );
     }
 
 }
 
 const styles = StyleSheet.create({
-    scrollView: {
-        backgroundColor: "#fff",
-        paddingHorizontal:20
-    },
-    mainView: {
-        flexDirection: "row",
-        width: "100%",
-        marginTop: 40,
-        alignItems: "center",
-        justifyContent: 'space-between'
-        
-    },
-    postavkeIKonf: {
-        width: "80%"
-    },
-    settingsText: {
-        fontSize:30,
-        fontWeight: "normal"
-    },
-    settingsTextName: {
-        fontSize:30,
-        fontWeight: "700"
-    },
-    profileIconView: {
-        width: "20%",
-        alignItems: "flex-end"
-    },
-    profileIconImage: {
-        width:50,
-        height:50,
-		borderRadius: 50 / 2,
-    },
-    sortView: {
-        alignItems:"center",
-        elevation:2,
-        width:"15%",
-        backgroundColor:"#FFF",
-        marginLeft:5,
-        height:35,
-        borderRadius:10,
-        marginLeft:1,
-        justifyContent:"center"
-    },
-    requestView: {
-        flexDirection: "column",
-        width: "100%",
-        marginTop: 40,
-        justifyContent: 'space-between'
-    },
-    imageCenter: {
-        alignItems:"center",
-        justifyContent:"center",
-        width:55,
-        height:55,
-        
-
-    },
-    imeISlika:{
-        alignItems:"center",
-        justifyContent:"center", 
-        
-         
-    },
-    margineTeksta:{
-        marginBottom:0,
-        marginTop:20,
-        marginLeft:20,
-        bottom:40
-    },
-    marginaSlikeIokvir:{
-        marginBottom:40,
-        marginTop:80,
-        borderRadius:22,
-        borderWidth:2,
-        borderColor:"grey",
-        height:610,
-        justifyContent: "center",
-        bottom:50,
-    },
-    tekstImena:{
-        marginBottom:20,
-        marginTop:4,
-        fontSize:25,
-        fontWeight: "700",
-    },
-    tekstIzbornika:{
-        fontWeight: "bold",
-        fontSize:18,  
-        left:28,
-        top:-5,
-    },
-    foi:{
-        alignItems:"center",
-        flexDirection: 'row',
-        justifyContent: 'center',
-        bottom:0, 
-    },
-    Switch1:{
-        left:80,
-    },
-    arrow:{
-        left:300,
-        bottom:22, 
-    },
-    logoutIcon:{
-       top:20, 
-    },
-    darkMode:{
-        top:35,
-        right:5,
-    },
-    switchPlace1:{
-        marginLeft:282,
-        right:8
-    },
-    switchPlace2:{
-        marginLeft:282,
-        bottom:26,
-        right:8
-    },
-    lightUp:{
-        top:55,
-        right:2
-    },
-    notificationIcon:{
-        top:20,
-        right:2,
-    },
-    notificationTxt:{
-        right:5,
-    },
-    emailStavke:{
-        bottom:18,
-    },
-    dodajUkloniStavke:{
-        top:10,
-    }
-  
-})
+  scrollView: {
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+  },
+  mainView: {
+    flexDirection: "row",
+    width: "100%",
+    marginTop: 0,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  postavkeIKonf: {
+    width: "80%",
+  },
+  settingsText: {
+    fontSize: 30,
+    fontWeight: "normal",
+  },
+  settingsTextName: {
+    fontSize: 30,
+    fontWeight: "700",
+  },
+  profileIconView: {
+    width: "20%",
+    alignItems: "flex-end",
+  },
+  profileIconImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50 / 2,
+  },
+  sortView: {
+    alignItems: "center",
+    elevation: 2,
+    width: "15%",
+    backgroundColor: "#FFF",
+    marginLeft: 5,
+    height: 35,
+    borderRadius: 10,
+    marginLeft: 1,
+    justifyContent: "center",
+  },
+  requestView: {
+    flexDirection: "column",
+    width: "100%",
+    marginTop: 40,
+    justifyContent: "space-between",
+  },
+  imageCenter: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 55,
+    height: 55,
+  },
+  imeISlika: {
+    alignItems: "center",
+    justifyContent: "center",
+    bottom: 45,
+  },
+  margineTeksta: {
+    marginBottom: 0,
+    marginTop: 20,
+    marginLeft: 20,
+    bottom: 85,
+  },
+  marginaSlikeIokvir: {
+    marginBottom: 40,
+    marginTop: 80,
+    borderRadius: 22,
+    borderWidth: 2,
+    borderColor: "grey",
+    height: 680,
+    justifyContent: "center",
+    bottom: 50,
+  },
+  tekstImena: {
+    marginBottom: 20,
+    marginTop: 4,
+    fontSize: 25,
+    fontWeight: "700",
+  },
+  tekstIzbornika: {
+    fontWeight: "bold",
+    fontSize: 18,
+    left: 28,
+    top: -5,
+  },
+  foi: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    bottom: 350,
+  },
+  Switch1: {
+    left: 80,
+  },
+  arrow: {
+    left: 290,
+    bottom: 22,
+  },
+  logoutIcon: {
+    top: 20,
+  },
+  darkMode: {
+    top: 35,
+    right: 5,
+  },
+  switchPlace1: {
+    marginLeft: 282,
+    right: 8,
+  },
+  switchPlace2: {
+    marginLeft: 282,
+    bottom: 26,
+    right: 8,
+  },
+  lightUp: {
+    top: 55,
+    right: 2,
+  },
+  notificationIcon: {
+    top: 20,
+    right: 2,
+  },
+  notificationTxt: {
+    right: 5,
+  },
+  emailStavke: {
+    bottom: 18,
+  },
+  dodajUkloniStavke: {
+    top: 10,
+  },
+  margineOdjave: {
+    top: 70,
+    left: 22,
+    width: 100,
+  },
+  txtButtonIcon: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  btn1: {
+    right: -2,
+    bottom: 320,
+  },
+  btn2: {
+    right: 54,
+    bottom: 320,
+  },
+  btnBorder1: {
+    top: 35,
+    borderColor: colors.black,
+    backgroundColor: colors.white,
+    borderRadius: 8,
+    borderWidth: 2,
+    padding: 8,
+    width: "160%",
+    height: "20%",
+    left: 2,
+  },
+  btnBorder2: {
+    top: 35,
+    borderColor: colors.black,
+    backgroundColor: colors.white,
+    borderRadius: 8,
+    borderWidth: 2,
+    padding: 8,
+    width: "160%",
+    height: "20%",
+    left: 7,
+  },
+  btnText1: {
+    fontWeight: "bold",
+    fontSize: 12,
+    left: 24,
+    bottom: 0,
+    fontSize: 13,
+  },
+  btnText2: {
+    fontWeight: "bold",
+    fontSize: 12,
+    left: 24,
+    bottom: 0,
+    fontSize: 13,
+  },
+  dizajnKarticaTekst: {
+    bottom: 315,
+    textAlign: "center",
+    //fontWeight: "bold",
+    fontSize: 17,
+  },
+  btnNormal: {
+    borderColor: "blue",
+    borderWidth: 1,
+    borderRadius: 10,
+    height: 30,
+    width: 100,
+  },
+  btnPress: {
+    borderColor: "blue",
+    borderWidth: 1,
+    height: 30,
+    width: 100,
+  },
+});
